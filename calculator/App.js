efbd7233a1e70c96cba251b05e34e23ba156f1e4
wonjae <wonjae0709@jinhakapply.com>
2022-07-07 10:09:40 +0900

    make buttons round and change colors, methods

--- calculator/App.js
+++ calculator/App.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
+import { StyleSheet, View, Button, Text, TouchableOpacity } from 'react-native';
 
 class App extends Component {
   constructor(props) {
@@ -147,16 +147,59 @@ class App extends Component {
           number: this.state.number + val,
         });
       }
-    } else if (val === "AC") {
+    } else if (val === 'AC') {
       this.setState({
         number: '0',
       });
     }
   };
 
-  allButton = ({title}) => {
-    return(<Button title={title}  onPress={() => this.makeNumbers(title)} style={styles.button}></Button>);
-  }
+  allButton = ({ title }) => {
+    if (title === '=') {
+      return (
+        <TouchableOpacity
+          style={styles.roundButtonEqual}
+          onPress={this.makeSolution}>
+          <Text style={styles.textButtons}>{title}</Text>
+        </TouchableOpacity>
+      );
+    } else if (
+      title === '()' ||
+      title === '%' ||
+      title === '/' ||
+      title === 'X' ||
+      title === '-' ||
+      title === '+'
+    ) {
+      return (
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => {
+            this.makeNumbers(title);
+          }}>
+          <Text style={styles.textButtonSign}>{title}</Text>
+        </TouchableOpacity>
+      );
+    } else if (title === 'AC') {
+      return (
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => {
+            this.makeNumbers(title);
+          }}>
+          <Text style={styles.textButtonAC}>{title}</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => this.makeNumbers(title)}>
+          <Text style={styles.textButtons}>{title}</Text>
+        </TouchableOpacity>
+      );
+    }
+  };
 
   render() {
     return (
@@ -166,35 +209,35 @@ class App extends Component {
         </View>
         <View style={styles.downPlace}>
           <View style={styles.buttons}>
-            <this.allButton title="AC" ></this.allButton>
-            <this.allButton title="()" ></this.allButton>
-            
-            <this.allButton title="%" ></this.allButton>
-            <this.allButton title="/" ></this.allButton>
+            <this.allButton title="AC"></this.allButton>
+            <this.allButton title="()"></this.allButton>
+
+            <this.allButton title="%"></this.allButton>
+            <this.allButton title="/"></this.allButton>
           </View>
-          <View style={styles.buttons}>
-            <this.allButton title="7" ></this.allButton>
-            <this.allButton title="8" ></this.allButton>
-            <this.allButton title="9" ></this.allButton>
-            <this.allButton title="X" ></this.allButton>
+          <View style={styles.buttons2}>
+            <this.allButton title="7"></this.allButton>
+            <this.allButton title="8"></this.allButton>
+            <this.allButton title="9"></this.allButton>
+            <this.allButton title="X"></this.allButton>
           </View>
-          <View style={[styles.buttons]}>
-            <this.allButton title="4" ></this.allButton>
-            <this.allButton title="5" ></this.allButton>
-            <this.allButton title="6" ></this.allButton>
-            <this.allButton title="-" ></this.allButton>
+          <View style={styles.buttons3}>
+            <this.allButton title="4"></this.allButton>
+            <this.allButton title="5"></this.allButton>
+            <this.allButton title="6"></this.allButton>
+            <this.allButton title="-"></this.allButton>
           </View>
-          <View style={styles.buttons}>
-            <this.allButton title="1" ></this.allButton>
-            <this.allButton title="2" ></this.allButton>
-            <this.allButton title="3" ></this.allButton>
-            <this.allButton title="+" ></this.allButton>
+          <View style={styles.buttons4}>
+            <this.allButton title="1"></this.allButton>
+            <this.allButton title="2"></this.allButton>
+            <this.allButton title="3"></this.allButton>
+            <this.allButton title="+"></this.allButton>
           </View>
-          <View style={[styles.buttons]}>
-            <this.allButton title="+/-" ></this.allButton>
-            <this.allButton title="0" ></this.allButton>
-            <this.allButton title="." ></this.allButton>
-            <this.allButton title="=" ></this.allButton>
+          <View style={styles.buttons5}>
+            <this.allButton title="+/-"></this.allButton>
+            <this.allButton title="0"></this.allButton>
+            <this.allButton title="."></this.allButton>
+            <this.allButton title="="></this.allButton>
           </View>
         </View>
       </View>
@@ -208,13 +251,13 @@ const styles = StyleSheet.create({
   },
   upperPlace: {
     flex: 0.3,
-    backgroundColor: 'gray',
+    backgroundColor: 'white',
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
   },
   downPlace: {
     flex: 0.8,
-    backgroundColor: 'yellow',
+    backgroundColor: 'black',
   },
   buttons: {
     flex: 0.1,
@@ -224,12 +267,71 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 23,
   },
+  buttons2: {
+    flex: 0.1,
+    flexDirection: 'row',
+    marginTop: 70,
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 23,
+  },
+  buttons3: {
+    flex: 0.1,
+    flexDirection: 'row',
+    marginTop: 70,
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 23,
+  },
+  buttons4: {
+    flex: 0.1,
+    flexDirection: 'row',
+    marginTop: 70,
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 23,
+  },
+  buttons5: {
+    flex: 0.1,
+    flexDirection: 'row',
+    marginTop: 70,
+    justifyContent: 'space-between',
+    marginLeft: 20,
+    marginRight: 23,
+  },
   calculatedNumber: {
     fontSize: 50,
   },
-  button: {
-    color: "red"
-  }
+  roundButton: {
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#292929',
+  },
+  roundButtonEqual: {
+    width: 70,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: 'green',
+  },
+  textButtons: {
+    color: 'white',
+    fontSize: 30,
+  },
+  textButtonSign: {
+    color: 'green',
+    fontSize: 30,
+  },
+  textButtonAC: {
+    color: 'red',
+    fontSize: 30,
+  },
 });
 
 export default App;
